ff264b81bce90cd494e5bd344fdda711318e4b47
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-11 23:28:58 +0200

    create test sum

---
+++ src/test/java/com/codurance/katalyst/OperationTest.java
@@ -0,0 +1,13 @@
+package com.codurance.katalyst;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class OperationTest {
+    
+    @Test
+    public void solve_sum(){
+        assertEquals(12, Operation.create("(3 + 9)").eval());
+    }
+}
