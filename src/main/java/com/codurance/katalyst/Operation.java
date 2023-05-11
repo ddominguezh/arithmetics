7864f05182e133c0e13a0425fb5dd47254f77434
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-11 23:29:34 +0200

    move operation class, create sum

---
+++ src/main/java/com/codurance/katalyst/Operation.java
@@ -0,0 +1,20 @@
+package com.codurance.katalyst;
+
+public class Operation {
+
+    private float leftOperand;
+    private String simbol;
+    private float rightOperand;
+    protected Operation(float leftOperand, String simbol, float rightOperand){
+        this.leftOperand = leftOperand;
+        this.simbol = simbol;
+        this.rightOperand = rightOperand;
+    }
+    public static Operation create(String operation) {
+        String[] data = operation.substring(1, operation.length()-1).split(" ", -1);
+        return new Operation(Float.valueOf(data[0]), data[1], Float.valueOf(data[2]));
+    }
+    public float eval(){
+        return leftOperand + rightOperand;
+    }
+}
