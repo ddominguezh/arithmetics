c5a075089d6f0a6a2b90b751640be7e396825bd0
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-11 23:33:16 +0200

    create test multiplication

--- src/test/java/com/codurance/katalyst/OperationTest.java
+++ src/test/java/com/codurance/katalyst/OperationTest.java
@@ -10,4 +10,8 @@ public class OperationTest {
     public void solve_sum(){
         assertEquals(12, Operation.create("(3 + 9)").eval());
     }
+
+    @Test void solve_multiplication(){
+        assertEquals(27, Operation.create("(3 * 9)").eval());
+    }
 }
