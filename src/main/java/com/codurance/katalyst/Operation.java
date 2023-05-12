64c2bf1a15d197e7ebec397a9e447d03c2292e8d
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-12 05:01:30 +0200

    refactor: creact hashmap with arithmetics operations

--- src/main/java/com/codurance/katalyst/Operation.java
+++ src/main/java/com/codurance/katalyst/Operation.java
@@ -1,13 +1,24 @@
 package com.codurance.katalyst;
 
+import java.util.HashMap;
+import java.util.function.BinaryOperator;
+
 public class Operation {
 
     private float leftOperand;
-    private String simbol;
+    private String symbol;
     private float rightOperand;
-    protected Operation(float leftOperand, String simbol, float rightOperand){
+    private HashMap<String, BinaryOperator<Float>> operations = new HashMap<String, BinaryOperator<Float>>(){
+        {
+            put("+", (left, right) -> left + right);
+            put("*", (left, right) -> left * right);
+            put("/", (left, right) -> left / right);
+            put("-", (left, right) -> left - right);
+        }
+    };
+    protected Operation(float leftOperand, String symbol, float rightOperand){
         this.leftOperand = leftOperand;
-        this.simbol = simbol;
+        this.symbol = symbol;
         this.rightOperand = rightOperand;
     }
     public static Operation create(String operation) {
@@ -15,15 +26,6 @@ public class Operation {
         return new Operation(Float.valueOf(data[0]), data[1], Float.valueOf(data[2]));
     }
     public float eval(){
-        if("*".equals(simbol)){
-            return leftOperand * rightOperand;
-        }
-        if("/".equals(simbol)){
-            return leftOperand / rightOperand;
-        }
-        if("-".equals(simbol)){
-            return leftOperand - rightOperand;
-        }
-        return leftOperand + rightOperand;
+        return operations.get(symbol).apply(leftOperand, rightOperand);
     }
 }
