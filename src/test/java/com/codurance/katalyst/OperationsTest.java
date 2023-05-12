8d6920ff846c48c9f2ff38d5a1e134d90dfe3678
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-12 06:57:05 +0200

    return value when operations is only number

--- src/test/java/com/codurance/katalyst/OperationsTest.java
+++ src/test/java/com/codurance/katalyst/OperationsTest.java
@@ -13,7 +13,11 @@ public class OperationsTest {
 
     @Test
     public void return_value_when_operations_is_only_number_between_bracket(){
-        assertEquals(14, Operations.create("(14)").eval());
+        assertEquals(14, Operations.create("( 14 )").eval());
     }
     
+    @Test
+    public void return_value_when_operations_is_only_number(){
+        assertEquals(14, Operations.create("14").eval());
+    }
 }
