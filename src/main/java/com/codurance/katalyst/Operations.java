0548503a4d58a31120328983ef1c66ef38bce0e5
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-12 06:50:04 +0200

    refactor: extract method is only bracket

--- src/main/java/com/codurance/katalyst/Operations.java
+++ src/main/java/com/codurance/katalyst/Operations.java
@@ -1,5 +1,10 @@
 package com.codurance.katalyst;
 
+import java.util.Optional;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Operations {
 
     private String operations;
@@ -11,7 +16,15 @@ public class Operations {
     }
 
     public Integer eval() {
+        if(isOnlyBracket()){
+            return 0;
+        }
         return 0;
     }
     
+    private boolean isOnlyBracket(){
+        Matcher matcher = Pattern.compile("[\\(\\)]+").matcher(operations);
+        Optional<MatchResult> result = matcher.results().findFirst();
+        return result.isPresent() && operations.equals(result.get().group());
+    }
 }
