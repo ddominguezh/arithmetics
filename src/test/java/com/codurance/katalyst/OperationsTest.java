4bb30e4ae73608e0d9877b0c47761811b0fc266f
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-12 06:34:26 +0200

    create test: return value when operations is only number between bracked

--- src/test/java/com/codurance/katalyst/OperationsTest.java
+++ src/test/java/com/codurance/katalyst/OperationsTest.java
@@ -10,4 +10,10 @@ public class OperationsTest {
     public void return_zero_when_operations_are_only_brackets(){
         assertEquals(0, Operations.create("((()()))").eval());
     }
+
+    @Test
+    public void return_value_when_operations_is_only_number_between_bracket(){
+        assertEquals(14, Operations.create("(14)").eval());
+    }
+    
 }
