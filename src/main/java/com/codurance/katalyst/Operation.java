aa572635446dc0295626297a479bce023fc8d965
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-12 04:31:20 +0200

    add the division

--- src/main/java/com/codurance/katalyst/Operation.java
+++ src/main/java/com/codurance/katalyst/Operation.java
@@ -18,6 +18,9 @@ public class Operation {
         if("*".equals(simbol)){
             return leftOperand * rightOperand;
         }
+        if("/".equals(simbol)){
+            return leftOperand / rightOperand;
+        }
         return leftOperand + rightOperand;
     }
 }
